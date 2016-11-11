13d0a3952b5f91c9b2f6c1bbe2d207bf114768ae
Joel Gugger <joel@thimoo.ch>
2016-11-11 14:01:40 +0100

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swissMetNetDisplayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the swissMetNetDisplayApp
+ */
+angular.module('swissMetNetDisplayApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name swissMetNetDisplayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the swissMetNetDisplayApp
+ */
+angular.module('swissMetNetDisplayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

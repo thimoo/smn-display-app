33e196f8378443f89a58596b6e0b166d999bb7a9
Joel Gugger <joel@thimoo.ch>
2016-11-29 15:00:51 +0100

    fade effect slider

--- app/scripts/controllers/graphicslider.js
+++ app/scripts/controllers/graphicslider.js
@@ -12,16 +12,16 @@ angular.module('swissMetNetDisplayApp')
     
     var counter = 0;
 
-    $scope.g1 = true;
-    $scope.g2 = true;
+    $scope.disabled1 = true;
+    $scope.disabled2 = false;
 
     $interval(function() {
 
       counter++;
       counter %= 2;
 
-      $scope.g1 = (counter == 0);
-      $scope.g2 = (counter == 1);
+      $scope.disabled1 = (counter == 0);
+      $scope.disabled2 = (counter == 1);
 
 
     }, 5000);

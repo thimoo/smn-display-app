26680d5041b2822a728055394e8bd5bc62b9d7c6
Joel Gugger <joel@thimoo.ch>
2016-11-29 11:12:40 +0100

    add basic graphic

--- app/scripts/directives/graphictemp.js
+++ app/scripts/directives/graphictemp.js
@@ -19,8 +19,14 @@ angular.module('swissMetNetDisplayApp')
           // If the targeted directive is not this
           // skip the update
           if (data.target.indexOf('graphicTemp') === -1) { return; }
+          
+          console.log('update graphic temp');
 
-          // TODO
+          var url = data.data.collections.temp;
+
+          webService.get(url, function (data) {
+            console.log(data);
+          });
 
         });
 

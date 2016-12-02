7ef11e04ec5811fdae3853ae8eba32f99c435edf
Joel Gugger <joel@thimoo.ch>
2016-12-02 13:36:27 +0100

    animate qnh

--- app/scripts/directives/singleqnh.js
+++ app/scripts/directives/singleqnh.js
@@ -15,7 +15,20 @@ angular.module('swissMetNetDisplayApp')
 
       controller: function ($scope, webService) {
 
-        $scope.hpa = '-';
+        var minQnh = 990;
+        var maxQnh = 1040;
+        var diff = -40 - 90;
+        var dist = 180;
+        var oldTo = 0;
+
+        $scope.hpa = 0;
+
+        // 50 hPa
+        // 0 -> 990
+        // 50 -> 1040
+        // 
+        // 1017.8 -> 27.8
+        // 
 
         $scope.$on('update', function (event, data) {
           // If the targeted directive is not this
@@ -26,7 +39,23 @@ angular.module('swissMetNetDisplayApp')
           var url = data.data.data.qnh;
 
           webService.get(url, function (data) {
+
             $scope.hpa = data.value;
+
+            var from = oldTo;
+            var to = ((maxQnh - $scope.hpa - (maxQnh - minQnh)) * -1) / (maxQnh - minQnh) * dist + diff;
+
+            var svg = d3.select("#arrow-gnh");
+
+            svg.transition().duration(1000).attrTween('transform', rotTween);
+            
+            function rotTween() {
+              var i = d3.interpolate(from, to);
+              return function(t) {
+                  return "rotate(" + i(t) + " 50.867 47.148)";
+              };
+            }
+
           });
 
         });
